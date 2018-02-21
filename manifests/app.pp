1eb6e38e010b072363c1c3fe2a061ca6082e0ff1
Mariano Beccaria <vinoveritas40@gmail.com>
2018-02-21 15:03:39 +0000

    Added node3

--- manifests/app.pp
+++ manifests/app.pp
@@ -1,5 +1,4 @@
-node 'node1' {
-    include base
+node 'node3' {
     
     class { '::tomcat' :
         user    => 'tomcat',
@@ -10,28 +9,8 @@ node 'node1' {
     }
     
     tomcat::deploy { "sysfoo":
-        # deploy_url      => "https://3-122260486-gh.circle-artifacts.com/0/tmp/circle-artifacts.8hsrwKe/sysfoo.war",
-        # checksum_value  => "8b68b71a1d1f8274e27d1ea83a46a56e"
         deploy_url      => "https://1-122260486-gh.circle-artifacts.com/0/tmp/circle-artifacts.pVnnnJ3/sysfoo.war",
         checksum_value  => "69b3e62429838c3fd742f4dcdd08795e",
     }
     
-}
-
-node 'node2' {
-
-    include base
-
-    class { '::tomcat' :
-        user    => 'tomcat',
-        group   => 'tomcat',
-        xms     => '70m',
-        xmx     => '130m',
-        service_state   => 'running',
-    }
-    
-    tomcat::deploy { "sysfoo":
-        deploy_url      => "https://3-122260486-gh.circle-artifacts.com/0/tmp/circle-artifacts.8hsrwKe/sysfoo.war",
-        checksum_value  => "8b68b71a1d1f8274e27d1ea83a46a56e",
-    }
 }
